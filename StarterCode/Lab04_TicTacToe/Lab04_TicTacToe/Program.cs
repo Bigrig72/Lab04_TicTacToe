08dba862d9fe8d9832b516bc4b69583af0396c9d
Regan Dufort <regan.t.dufort@gmail.com>
2019-01-13 21:39:07 -0800

    added board display, game functionality

---
+++ StarterCode/Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Lab04_TicTacToe.Classes;
+
+namespace Lab04_TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Player player1 = new Player();
+            player1.Name = "Regan";
+            player1.Marker = "O";
+            player1.IsTurn = true;
+
+            Player player2 = new Player();
+            player2.Name = "Lawurence";
+            player2.Marker = "X";
+            player2.IsTurn = false;
+
+            Game newGame = new Game(player1,player2);
+            newGame.Play();
+            
+
+            Console.ReadKey();
+        
+        
+        }
+    }
+}
